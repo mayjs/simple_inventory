88c291d038b07893f0d254a51eb48341bf0695b8
may <johannes.may@udo.edu>
2020-04-27 09:03:30 +0200

    Added meta html tags

--- web/inventory_web.py
+++ web/inventory_web.py
@@ -13,6 +13,8 @@ def mini_html(body):
     return f"""
         <!DOCTYPE html>
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
         </head>
         <body>
             {body}
